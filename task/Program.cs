0a60cd81c51bfde0587f23b3fc63c56f4e1a4f4a
Shahnaz436 <47938493+Shahnaz436@users.noreply.github.com>
2020-05-09 12:36:51 +0400

    Add project files.

---
+++ task/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Authorname", "Titlename", "Contentname");
+            book.Show();
+        }
+    }
+
+    class Book
+    {
+        Author author1;
+        Title title1;
+        Content content1;
+        public Book(string author, string title,string content)
+        {
+             author1 = new Author(author);
+             title1 = new Title(title);
+             content1 = new Content(content);
+        }
+
+        public void Show()
+        {
+            author1.Show();
+            title1.Show();
+            content1.Show();
+        }
+    }
+
+    class Author
+    {
+        private string author;
+
+        public Author(string author)
+        {
+            this.author = author;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Author: {author}");
+        }
+    }
+
+    class Content
+    {
+        private string content;
+
+        public Content(string content)
+        {
+            this.content = content;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Content: {content}");
+        }
+    }
+
+    class Title
+    {
+        private string title;
+
+        public Title(string title)
+        {
+            this.title = title;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Title: {title}");
+        }
+    }
+}
